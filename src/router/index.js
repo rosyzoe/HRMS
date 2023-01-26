209ec0c134b74b617ea948524034636f3cf16b91
coderzxx <1285367184@qq.com>
2023-01-26 23:45:38 +0800

    路由-模块化管理动态路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,15 @@ Vue.use(Router)
 
 /* Layout */
 import Layout from '@/layout'
+// 引入多个动态路由模块
+import approvalsRouter from './modules/approvals'
+import departmentsRouter from './modules/departments'
+import employeesRouter from './modules/employees'
+import permissionRouter from './modules/permission'
+import attendancesRouter from './modules/attendances'
+import salarysRouter from './modules/salarys'
+import settingRouter from './modules/setting'
+import socialRouter from './modules/social'
 
 // 静态路由表：不需要做权限控制的路由，每个用户都可以正常访问。
 export const constantRoutes = [
@@ -41,102 +50,14 @@ export const constantRoutes = [
 // 动态路由表，项目中不同的用户可以访问不同的功能
 export const asyncRoutes = [
   // 动态路由规则
-  {
-    path: '/approvals', // 审批
-    component: Layout,
-    children: [
-      {
-        path: '',
-        name: 'Approvals',
-        component: () => import('@/views/approvals'),
-        meta: { title: '审批' }
-      }
-    ]
-  },
-  {
-    path: '/attendances', // 考勤
-    component: Layout,
-    children: [
-      {
-        path: '',
-        name: 'Attendances',
-        component: () => import('@/views/attendances'),
-        meta: { title: '考勤' }
-      }
-    ]
-  },
-  {
-    path: '/departments', // 组织架构
-    component: Layout,
-    children: [
-      {
-        path: '',
-        name: 'Departments',
-        component: () => import('@/views/departments'),
-        meta: { title: '组织架构' }
-      }
-    ]
-  },
-  {
-    path: '/employees', // 员工
-    component: Layout,
-    children: [
-      {
-        path: '',
-        name: 'Employees',
-        component: () => import('@/views/employees'),
-        meta: { title: '员工' }
-      }
-    ]
-  },
-  {
-    path: '/permission', // 权限
-    component: Layout,
-    children: [
-      {
-        path: '',
-        name: 'Permission',
-        component: () => import('@/views/permission'),
-        meta: { title: '权限' }
-      }
-    ]
-  },
-  {
-    path: '/salarys', // 薪资
-    component: Layout,
-    children: [
-      {
-        path: '',
-        name: 'Salarys',
-        component: () => import('@/views/salarys'),
-        meta: { title: '薪资' }
-      }
-    ]
-  },
-  {
-    path: '/setting', // 角色设置
-    component: Layout,
-    children: [
-      {
-        path: '',
-        name: 'Setting',
-        component: () => import('@/views/setting'),
-        meta: { title: '角色设置' }
-      }
-    ]
-  },
-  {
-    path: '/social', // 社保
-    component: Layout,
-    children: [
-      {
-        path: '',
-        name: 'Social',
-        component: () => import('@/views/social'),
-        meta: { title: '社保' }
-      }
-    ]
-  }
+  departmentsRouter,
+  settingRouter,
+  employeesRouter,
+  permissionRouter,
+  approvalsRouter,
+  attendancesRouter,
+  salarysRouter,
+  socialRouter
 ]
 
 const createRouter = () =>
