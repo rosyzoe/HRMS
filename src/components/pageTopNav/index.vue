84f7b435df50550648f5bed589b16f28921e7624
coderzxx <1285367184@qq.com>
2023-01-29 14:59:47 +0800

    员工管理-公用的头部组件并在员工组件中使用

---
+++ src/components/pageTopNav/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="page-top-nav">
+    <el-card>
+      <el-row type="flex" justify="space-between" align="middle">
+        <!-- 左侧文字提示区域 -->
+        <el-col>
+          <el-alert class="tip" type="success" :closable="false" show-icon>
+            <template #title>
+              <slot name="left">我是左侧的默认标题</slot>
+            </template>
+          </el-alert>
+        </el-col>
+        <!-- 右侧按钮区域 -->
+        <el-col>
+          <el-row type="flex" justify="end">
+            <slot name="right"> 我是右侧的默认内容 </slot>
+          </el-row>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageTopNav',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.tip {
+  width: 200px;
+  border: 1px solid #e6f7ff;
+  font-weight: bold;
+}
+</style>
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="employees-container">
     <div class="app-container">
+      <!-- 顶部区域 -->
+      <page-top-nav>
+        <!-- 左侧标题 -->
+        <template #left> 1 </template>
+
+        <!-- 右侧按钮 -->
+        <template #right>
+          <el-button type="danger" size="small">简单表头导出</el-button>
+          <el-button type="info" size="small">复杂表头导出</el-button>
+          <el-button type="warning" size="small">excel导入</el-button>
+          <el-button type="primary" size="small">新增员工</el-button>
+        </template>
+      </page-top-nav>
       <h2>员工</h2>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import pageTopNav from '@/components/pageTopNav'
+export default {
+  components: {
+    pageTopNav
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
